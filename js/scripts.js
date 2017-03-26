939820331baf45d1b3db691bbf716539a86a0ea6
Krzysztof Barański <Krzysztof Baraski>
2017-03-27 00:06:39 +0200

    DOM poprawione

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 // plik scripts.js
 
-var buttonText = document.getElementsByClassName("button");
+var buttonText = document.getElementsByClassName('button');
 var j = (buttonText.length);
-for ( var k = 0 ; k < j ; k++) {
+for (var k = 0; k < j; k++) {
+  alert('wartoœæ buttonText.innerText to: ' + buttonText[k].innerText);
   console.log('wartoœæ k teraz to: ' + k + ' z '+ j);
   console.log('wartoœæ buttonText.innerText to: ' + buttonText[k].innerText);
-} 
+  console.log(buttonText);
+}
