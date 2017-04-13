55170a8090947c2a8e7a30f32eff2a59af34ee83
Krzysztof Barański <Krzysztof Baraski>
2017-04-13 15:34:01 +0200

    ptaszek

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 var list = document.getElementById('list');
 var button = document.getElementById('addElem');
 button.addEventListener('click', function() {
-  var x = list.getElementsByTagName('LI');
+  var x = list.getElementsByTagName('li');
   var itemNumber = x.length;
-  list.innerHTML += '<li>item ' + itemNumber + '</li'
+  list.innerHTML += '<li>item ' + itemNumber + '</li>'
 });
